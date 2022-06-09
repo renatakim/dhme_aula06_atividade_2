6291550b3ce2f74dd85968d89e76e7f6d27afed8
cassiofreitas <cassiofreitas@cassiofreitas.com>
2022-06-08 21:48:21 -0300

    Adjusting README.md; adding first solution version

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # dhme_aula06_atividade_2
-dhme_aula06_atividade_2
+
+## Participantes (github username) com link para pasta contendo a solução
+- [ cassiofreitas ](./dhme_aula06_atividade_2_cassio)
+- [ renatakim ]()
+- [ santanagabi ]()
+- [ LuanME ]()
+- [ martinelli105 ]()
+
+## Respostas para o questionário da atividade
--- dhme_aula06_atividade_2_cassio/Program.cs
+++ dhme_aula06_atividade_2_cassio/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+const double umaMilha = 1.609344;
+
+Console.Write("Quantos KM o veículo percorreu? ");
+int quilometrosPercorridosPeloVeiculo = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(value: $"Seu veículo percorreu um total de {quilometrosPercorridosPeloVeiculo / umaMilha} milhas");
